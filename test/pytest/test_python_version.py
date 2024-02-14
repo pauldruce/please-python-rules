3c97c3754d7796f81be52dc3dbc8fad1dd3ec04e
Paul Druce <30435358+pauldruce@users.noreply.github.com>
2024-02-14 00:20:49 +0000

    try to make the test a bit more useful.

--- test/pytest/test_python_version.py
+++ test/pytest/test_python_version.py
@@ -1,4 +1,7 @@
 import sys
+import os 
 def test_python_version_is_expected():
-    assert sys.version_info[0] == 3
-    assert sys.version_info[1] == 10
+    version = os.environ['PYTHON_VERSION']
+    ver_nums = version.split('.')
+    assert sys.version_info[0] == ver_nums[0]
+    assert sys.version_info[1] == ver_nums[1]
