c2a72b140faf1d9735bdd9e2e9e79a693e1c6924
Paul Druce <30435358+pauldruce@users.noreply.github.com>
2024-02-14 00:22:45 +0000

    revert to using 3.12 - as that demonstrates the point.

--- test/pytest/test_python_version.py
+++ test/pytest/test_python_version.py
@@ -1,7 +1,4 @@
 import sys
-import os 
 def test_python_version_is_expected():
-    version = os.environ['PYTHON_VERSION']
-    ver_nums = version.split('.')
-    assert sys.version_info[0] == ver_nums[0]
-    assert sys.version_info[1] == ver_nums[1]
+    assert sys.version_info[0] == 3
+    assert sys.version_info[1] == 12
